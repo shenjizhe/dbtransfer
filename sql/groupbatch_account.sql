1a297bdea71629dabfd76fd7f238090959eec11b
shenjizhe <thirdlucky@126.com>
2020-07-26 00:32:52 +0800

    write to destination is complete!

---
+++ sql/groupbatch_account.sql
@@ -0,0 +1,6 @@
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (1, 1, 13);
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (2, 1, 12);
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (3, 1, 22);
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (4, 2, 12);
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (5, 3, 15);
+INSERT INTO `groupbatch_account`(`id`, `batch_id`, `balance`) VALUES (6, 2, 22);
---
+++ sql/groupbatch_copy.sql
@@ -0,0 +1,5 @@
+INSERT INTO `groupbatch_copy`(`id`, `totalBalance`, `count`) VALUES (1, 47, 3);
+INSERT INTO `groupbatch_copy`(`id`, `totalBalance`, `count`) VALUES (2, 34, 2);
+INSERT INTO `groupbatch_copy`(`id`, `totalBalance`, `count`) VALUES (3, 15, 1);
+INSERT INTO `groupbatch_copy`(`id`, `totalBalance`, `count`) VALUES (4, NULL, NULL);
+INSERT INTO `groupbatch_copy`(`id`, `totalBalance`, `count`) VALUES (5, NULL, NULL);
